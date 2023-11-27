5b6596815a3710bdcf3c7795d1b023aa16f2f025
sathishkavi1 <sathishkaviraj1@gmail.com>
2023-11-27 11:21:42 +0530

    First time push

---
+++ src/org/gits/TestA.java
@@ -0,0 +1,20 @@
+package org.gits;
+
+public class TestA {
+	
+	private void bike() {
+    System.out.println("Yamaha RX");
+
+	}
+	
+	private void car() {
+	System.out.println("Rolls Royce");
+
+	}
+	
+	private void plain() {
+	System.out.println("AirForce One");
+
+	}
+
+}
